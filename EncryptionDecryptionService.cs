4f6b6a15819f548e24814bbc84c2b71a1a588da8
Anisha Rungmahang Rai <1aanisha.rai@gmail.com>
2024-10-29 11:03:54 +0545

    feat: add RSA encryption service

---
+++ EncryptionDecryptionService.cs
@@ -0,0 +1,40 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace RSAEncryption
+{
+    public class EncryptionDecryptionService
+    {
+        public KeyPair GenerateKeyPair()
+        {
+            //2048 bits = 256 bytes
+            using var rsa = new RSACryptoServiceProvider(2048);
+            return new KeyPair
+            {
+                PublicKey = Convert.ToBase64String(rsa.ExportRSAPublicKey()),
+                PrivateKey = Convert.ToBase64String(rsa.ExportRSAPrivateKey())
+            };
+        }
+
+        public string Encrypt(string plainText, string publicKey)
+        {
+            //using doesnot require braces {} , object is disposed at the end of its scope
+            using var rsa = new RSACryptoServiceProvider();
+            rsa.ImportRSAPublicKey(Convert.FromBase64String(publicKey), out _);
+            var encryptedData = rsa.Encrypt(Encoding.UTF8.GetBytes(plainText), RSAEncryptionPadding.Pkcs1);
+            return Convert.ToBase64String(encryptedData);
+            // Code using rsa
+            // rsa.Dispose() is called automatically here, at the end of the method.
+        }
+
+
+        public string Decrypt(string cipherText, string privateKey)
+        {
+            using var rsa = new RSACryptoServiceProvider();
+            rsa.ImportRSAPrivateKey(Convert.FromBase64String(privateKey), out _);
+            var decryptedData = rsa.Decrypt(Convert.FromBase64String(cipherText), RSAEncryptionPadding.Pkcs1);
+            return Encoding.UTF8.GetString(decryptedData);
+
+        }
+    }
+}
---
+++ KeyPair.cs
@@ -0,0 +1,9 @@
+﻿
+namespace RSAEncryption
+{
+    public class KeyPair
+    {
+        public string PublicKey { get; set; }
+        public string PrivateKey { get; set; }
+    }
+}
